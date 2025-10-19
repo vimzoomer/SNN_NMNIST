a956d4baa474022f425e6a730a93bac970208d7f
Kacper Pelczarski <kacperpelczarski@Kacpers-MacBook-Pro-2.local>
2025-10-20 00:35:14 +0200

    README added

---
+++ README.md
@@ -0,0 +1,100 @@
+# SNN NMNIST
+
+This project implements Spiking Neural Networks (SNN) in PyTorch/Lava-DL and allows training on the NMNIST dataset. Based on the official lava-dl tutorials: https://github.com/lava-nc/lava-dl
+
+## Requirements
+
+* Python 3.10
+* Poetry 2.x
+* wget (for downloading NMNIST)
+* Jupyter Notebook / Jupyter Lab
+
+## Installation
+
+1. Clone the repository
+
+```
+git clone https://github.com/vimzoomer/SNN_NMNIST
+cd SNN_NMNIST
+```
+
+2. Install dependencies
+
+```
+poetry install
+```
+
+3. Activate the project environment
+
+Since Poetry 2.x, `poetry shell` is not installed by default. Use the following commands to activate the environment:
+
+```
+poetry env info --path
+source /path/to/venv/bin/activate  # replace with the path from previous command
+```
+
+## Preparing NMNIST Data
+
+By default, the data is downloaded automatically when the `NMNIST` class is used for the first time with `download=True`.
+To download manually:
+
+```
+wget https://www.dropbox.com/sh/tg2ljlbmtzygrag/AABlMOuR15ugeOxMCX0Pvoxga/Train.zip -P ./Train/ -q --show-progress
+wget 'https://www.dropbox.com/sh/tg2ljlbmtzygrag/AADSKgJ2CjaBWh75HnTNZyhca/Test.zip' -P ./Test/ -q --show-progress
+```
+
+## Running Training via Script
+
+The script `nmnist.py` accepts the following command-line arguments:
+
+* `--dir`: Dataset directory (default: `.`)
+* `--epochs`: Number of training epochs (default: 2)
+* `--batch_size`: Batch size for training and testing (default: 32)
+* `--sampling_time`: Sampling time for NMNIST spike events (default: 1)
+* `--sample_length`: Length of each sample in time bins (default: 300)
+* `--download`: Download NMNIST dataset if not present (flag, default: False)
+* `--augment`: Apply data augmentation to training data (flag, default: False)
+* `--load_model`: Path to pre-trained model to load before training (default: None)
+
+Example usage:
+
+```
+poetry run python ./nmnist.py --epochs 5 --batch_size 64 --download --augment --load_model
+```
+
+## Running Training in Jupyter Notebook
+
+1. Activate Poetry environment as described above.
+
+2. Launch Jupyter Notebook:
+
+```
+jupyter notebook snn_training.ipynb
+```
+
+3. The notebook already contains dataset setup, model initialization, training loops, and visualization. You can run the cells directly.
+
+## Visualizing Examples
+
+The code provides the `example_of_each_class` function in the `NMNIST` class, which generates a GIF animation showing examples of all classes and network activity:
+
+```python
+nmnist.example_of_each_class(net, merge_factor)
+```
+
+* `merge_factor`: An integer that decides how many time bins should be merged into a single frame. Increasing merge_factor reduces the number of frames and can make the animation smoother.
+
+The output will be saved as `vis_all_classes.gif`.
+
+## Dependencies
+
+The project uses the following packages:
+
+* lava-dl (from GitHub)
+* jupyter
+* notebook
+* ipykernel
+* matplotlib
+* numpy
+* torch
+* h5py
--- nmnist.py
+++ nmnist.py
@@ -1,17 +1,67 @@
+import argparse
 import glob
 import os
 import zipfile
 
 import h5py
+from lava.lib.dl.slayer.io import Event
 from matplotlib.animation import FuncAnimation, PillowWriter
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch import tensor
+from torch.utils.data import DataLoader, Dataset
 
 import lava.lib.dl.slayer as slayer
 
-def augment(event):
+
+class SNN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        neuron_params = {
+            'threshold': 1.25,
+            'current_decay': 0.25,
+            'voltage_decay': 0.03,
+            'tau_grad': 0.03,
+            'scale_grad': 3,
+            'requires_grad': True,
+        }
+
+        self.blocks = torch.nn.ModuleList(
+            [
+                slayer.block.cuba.Dense(
+                    neuron_params, 34*34*2, 64,
+                    weight_norm=True, delay=False
+                ),
+                slayer.block.cuba.Dense(
+                    neuron_params, 64, 128,
+                    weight_norm=True, delay=False
+                ),
+                slayer.block.cuba.Dense(
+                    neuron_params, 128, 64,
+                    weight_norm=True, delay=False
+                ),
+                slayer.block.cuba.Dense(
+                    neuron_params, 64, 10,
+                    weight_norm=True, delay=False
+                )
+            ]
+        )
+
+    def forward(self, spike):
+        for block in self.blocks:
+            spike = block(spike)
+        return spike
+
+    def export_hdf5(self, filename):
+        h = h5py.File(filename, 'w')
+        layer = h.create_group('layer')
+        for i, b in enumerate(self.blocks):
+            b.export_hdf5(layer.create_group(f'{i}'))
+
+
+def augment(event: Event) -> Event:
     x_shift = 4
     y_shift = 4
     theta = 10
@@ -24,8 +74,18 @@ def augment(event):
     event.y = event.x * sin_theta + event.y * cos_theta + yjitter
     return event
 
+
 class NMNIST(Dataset):
-    def __init__(self, dir, sampling_time, sample_length, transform=None, download=False, train=True):
+    num_classes = 10
+
+    def __init__(self,
+                 dir: str,
+                 sampling_time: int,
+                 sample_length: int,
+                 transform: callable = None,
+                 download: bool = False,
+                 train: bool = True
+                 ):
         self.dir = dir
 
         if train:
@@ -51,10 +111,10 @@ class NMNIST(Dataset):
         self.sampling_time = sampling_time
         self.sample_length = sample_length
         self.num_time_bins = int(sample_length / sampling_time)
-        self.data = [glob.glob(f'{self.data_path}/{digit}/*.bin') for digit in range(10)]
-        self.num_labels = len(self.data)
+        self.data = [(filename, int(filename.split('/')[-2]))  for filename in glob.glob(f'{self.data_path}/*/*.bin')]
+        self.data_by_class = [glob.glob(f'{self.data_path}/{digit}/*.bin') for digit in range(NMNIST.num_classes)]
 
-    def __read_spike(self, filename):
+    def __read_spike(self, filename: str) -> tensor:
         event = slayer.io.read_2d_spikes(filename)
 
         if self.transform is not None:
@@ -67,30 +127,23 @@ class NMNIST(Dataset):
 
         return spike
 
-    def __len__(self):
-        return sum(len(sublist) for sublist in self.data)
+    def __len__(self) -> int:
+        return len(self.data)
 
-    def __getitem__(self, idx):
-        flat_index = 0
-        filename = None
-        for sublist in self.data:
-            if idx < flat_index + len(sublist):
-                filename = sublist[idx - flat_index]
-                break
-            flat_index += len(sublist)
+    def __getitem__(self, idx: int) -> tuple[tensor, tensor]:
+        filename, label = self.data[idx]
+        return self.__read_spike(filename).reshape(-1, self.num_time_bins), label
 
-        return self.__read_spike(filename).reshape(-1, self.num_time_bins)
-
-    def example_of_each_class(self, net):
+    def example_of_each_class(self, net: SNN, merge_factor: int = 4) -> None:
         frames = []
         class_bars = []
         frame_labels = []
 
-        spikes = torch.cat([self.__read_spike(self.data[i][0]) for i in range(self.num_labels)], dim=-1)
-        spike_flattened = spikes.reshape(-1, self.num_labels * self.num_time_bins).unsqueeze(0)
-        classified, _ = net(spike_flattened)
+        spikes = torch.cat([self.__read_spike(self.data_by_class[i][0]) for i in range(NMNIST.num_classes)], dim=-1)
+        spike_flattened = spikes.reshape(-1, NMNIST.num_classes * self.num_time_bins).unsqueeze(0)
+        classified = net(spike_flattened)
 
-        for label in range(self.num_labels):
+        for label in range(NMNIST.num_classes):
             for t in range(label * self.num_time_bins, (label+1) * self.num_time_bins):
                 red_channel = spikes[1, :, :, t].numpy() * self.sampling_time
                 blue_channel = spikes[0, :, :, t].numpy() * self.sampling_time
@@ -107,13 +160,11 @@ class NMNIST(Dataset):
                 if spike_sum.item() > 0:
                     bar = (window.sum(dim=2) / spike_sum).detach().cpu().numpy()
                 else:
-                    bar = np.zeros((1, self.num_labels))
+                    bar = np.zeros((1, NMNIST.num_classes))
 
                 class_bars.append(bar)
 
                 frame_labels.append(label)
-
-        merge_factor = 4
 
         frames = [
             np.maximum.reduce(frames[i:i + merge_factor], axis=0)
@@ -148,65 +199,55 @@ class NMNIST(Dataset):
             return [im, im_class]
 
         ani = FuncAnimation(fig, update, frames=len(frames), interval=20, blit=True)
-        ani.save(f"vis_all_samples.gif", writer=PillowWriter(fps=30))
+        ani.save(f"vis_all_classes.gif", writer=PillowWriter(fps=30))
         plt.close(fig)
 
 
-class SNN(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        neuron_params = {
-            'threshold': 1.25,
-            'current_decay': 0.25,
-            'voltage_decay': 0.03,
-            'tau_grad': 0.03,
-            'scale_grad': 3,
-            'requires_grad': False,
-        }
-
-        self.blocks = torch.nn.ModuleList(
-            [
-                slayer.block.cuba.Dense(
-                    neuron_params, 34*34*2, 512,
-                    weight_norm=True, delay=False
-                ),
-                slayer.block.cuba.Dense(
-                    neuron_params, 512, 512,
-                    weight_norm=True, delay=False
-                ),
-                slayer.block.cuba.Dense(
-                    neuron_params, 512, 10,
-                    weight_norm=True, delay=False
-                )
-            ]
-        )
-
-    def forward(self, spike):
-        count = []
-        for block in self.blocks:
-            spike = block(spike)
-            count.append(torch.mean(spike).item())
-        return spike, torch.FloatTensor(count).reshape(
-            (1, -1)).to(spike.device)
-
-    def export_hdf5(self, filename):
-        h = h5py.File(filename, 'w')
-        layer = h.create_group('layer')
-        for i, b in enumerate(self.blocks):
-            b.export_hdf5(layer.create_group(f'{i}'))
-
-
 if __name__ == '__main__':
-    trained_dir = './Train'
-    nmnist = NMNIST(trained_dir, 1, 300)
+    parser = argparse.ArgumentParser(description="Train/test SNN on NMNIST")
+    parser.add_argument('--dir', type=str, default='.', help='Dataset directory')
+    parser.add_argument('--epochs', type=int, default=2, help='Number of training epochs')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
+    parser.add_argument('--sampling_time', type=int, default=1, help='Sampling time for NMNIST')
+    parser.add_argument('--sample_length', type=int, default=300, help='Sample length for NMNIST')
+    parser.add_argument('--download', action='store_true', help='Download NMNIST if not present')
+    parser.add_argument('--augment', action='store_true', help='Use data augmentation')
+    parser.add_argument('--load_model', type=str, default=None, help='Path to pre-trained model to load')
 
-    net = SNN()
+    args = parser.parse_args()
 
+    transform = augment if args.augment else None
+
+    nmnist = NMNIST(
+        dir=args.dir,
+        sampling_time=args.sampling_time,
+        sample_length=args.sample_length,
+        download=args.download,
+        transform=transform
+    )
+
+    nmnist_test = NMNIST(
+        dir=args.dir,
+        sampling_time=args.sampling_time,
+        sample_length=args.sample_length,
+        train=False
+    )
+
+    model = SNN()
+    if args.load_model is not None:
+        print(f"Loading pre-trained model from {args.load_model}")
+        state_dict = torch.load(args.load_model, map_location='cpu')
+        model.load_state_dict(state_dict)
+
+    net = model
     optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
     nmnist_loader = DataLoader(
-        dataset=nmnist, batch_size=32, shuffle=True
+        dataset=nmnist, batch_size=args.batch_size, shuffle=True
+    )
+
+    nmnist_test_loader = DataLoader(
+        dataset=nmnist_test, batch_size=args.batch_size, shuffle=False
     )
 
     error = slayer.loss.SpikeRate(
@@ -216,25 +257,27 @@ if __name__ == '__main__':
     stats = slayer.utils.LearningStats()
     assistant = slayer.utils.Assistant(
         net, error, optimizer, stats,
-        classifier=slayer.classifier.Rate.predict, count_log=True
+        classifier=slayer.classifier.Rate.predict
     )
 
-    epochs = 200
-
-    for epoch in range(epochs):
+    for epoch in range(args.epochs):
         for i, (input, label) in enumerate(nmnist_loader):
-            output, count = assistant.train(input, label)
+            output = assistant.train(input, label)
             header = [
-                'Event rate : ' +
-                ', '.join([f'{c.item():.4f}' for c in count.flatten()]),
+                'TRAIN\n' +
                 'Output : ' + str(output[0].sum(dim=1) / output[0].sum()),
                 'Label : ' + str(label[0])
             ]
             stats.print(epoch, iter=i, header=header, dataloader=nmnist_loader)
 
+        for i, (input, label) in enumerate(nmnist_test_loader):
+            output = assistant.test(input, label)
+            header = [
+                'TEST\n' +
+                'Output : ' + str(output[0].sum(dim=1) / output[0].sum()),
+                'Label : ' + str(label[0])
+            ]
+            stats.print(epoch, iter=i, header=header, dataloader=nmnist_test_loader)
+
+        torch.save(net.state_dict(), args.dir + '/network.pt')
         stats.update()
-
-
-
-
-
